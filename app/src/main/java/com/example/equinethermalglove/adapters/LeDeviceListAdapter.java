55842ad4890c6aea6ec05927d1f9a7aeebaa3b28
zpawliuk <zpawliuk@ualberta.ca>
2021-03-03 15:41:29 -0700

    Added to class 'bluetooth' (the service) and remade bluetoothReadIn while moving the old to bluetoothScan

--- app/src/main/java/com/example/equinethermalglove/adapters/LeDeviceListAdapter.java
+++ app/src/main/java/com/example/equinethermalglove/adapters/LeDeviceListAdapter.java
@@ -1,7 +1,6 @@
 package com.example.equinethermalglove.adapters;
 
 import android.bluetooth.BluetoothDevice;
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +8,6 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.example.equinethermalglove.R;
-import com.example.equinethermalglove.bluetoothReadIn;
 
 import java.util.ArrayList;
 
---
+++ app/src/main/java/com/example/equinethermalglove/bluetoothScan.java
@@ -0,0 +1,85 @@
+package com.example.equinethermalglove;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import com.example.equinethermalglove.adapters.LeDeviceListAdapter;
+
+public class bluetoothScan extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final long SCAN_PERIOD = 10000; // 10 second scanning period
+
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothLeScanner bluetoothLeScanner;
+    private boolean mScanning;
+    private Handler handler;
+    private LeDeviceListAdapter leDeviceListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_read_in);
+
+        // Make sure BLE is supported on the device
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // Get bluetooth Manager and Adapter
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Make sure bluetooth is enabled
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        bluetoothLeScanner = BluetoothAdapter.getDefaultAdapter().getBluetoothLeScanner();
+        handler = new Handler();
+        leDeviceListAdapter = new LeDeviceListAdapter(this.getLayoutInflater());
+    }
+
+    private void scanLeDevice() {
+        if (!mScanning) {
+            // Stops scanning after a pre-defined scan period.
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    bluetoothLeScanner.stopScan(leScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            bluetoothLeScanner.startScan(leScanCallback);
+        } else {
+            mScanning = false;
+            bluetoothLeScanner.stopScan(leScanCallback);
+        }
+    }
+
+    private ScanCallback leScanCallback =
+            new ScanCallback() {
+                @Override
+                public void onScanResult(int callbackType, ScanResult result) {
+                    super.onScanResult(callbackType, result);
+                    leDeviceListAdapter.addDevice(result.getDevice());
+                    leDeviceListAdapter.notifyDataSetChanged();
+                }
+            };
+}
